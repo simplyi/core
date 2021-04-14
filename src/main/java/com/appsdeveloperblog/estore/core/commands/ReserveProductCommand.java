c308c3da1310e3fa35639e4590c7e183e3255a04
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-04-14 17:26:22 -0400

    Created the ReserveProductCommand

---
+++ src/main/java/com/appsdeveloperblog/estore/core/commands/ReserveProductCommand.java
@@ -0,0 +1,19 @@
+package com.appsdeveloperblog.estore.core.commands;
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ReserveProductCommand {
+
+	@TargetAggregateIdentifier
+	private final String productId;
+	private final String quantity;
+	private final String orderId;
+	private final String userId;
+	
+	
+}
