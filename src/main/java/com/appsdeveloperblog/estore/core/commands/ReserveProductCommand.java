d9fc85c5788128d4989a9b5842b41f7a57be4a45
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-04-15 10:17:19 -0400

    Updated the datatype of the quantity field from String to int

--- src/main/java/com/appsdeveloperblog/estore/core/commands/ReserveProductCommand.java
+++ src/main/java/com/appsdeveloperblog/estore/core/commands/ReserveProductCommand.java
@@ -11,7 +11,7 @@ public class ReserveProductCommand {
 
 	@TargetAggregateIdentifier
 	private final String productId;
-	private final String quantity;
+	private final int quantity;
 	private final String orderId;
 	private final String userId;
 	
